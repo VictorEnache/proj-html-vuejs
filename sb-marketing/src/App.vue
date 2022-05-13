90c617315e93c9c792e8ff7b6496933b93dd0c55
VictorEnache <victor22enache@gmail.com>
2022-05-13 12:12:30 +0200

    create utility scss file and import it in App.vue

--- sb-marketing/src/App.vue
+++ sb-marketing/src/App.vue
@@ -17,6 +17,9 @@ export default {
 </script>
 
 <style lang="scss">
+
+@import  './assets/scss/utility.scss';
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
